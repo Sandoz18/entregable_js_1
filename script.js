2e4361a8ee6a1dc88a4691cb0a392cb4f7306bc2
Irene <ireneimdiez@gmail.com>
2024-12-26 19:53:37 -0300

    prueba ejercicio1

---
+++ script.js
@@ -0,0 +1,31 @@
+//declaro e inicializo la variable ofertas//
+const ofertas = document.getElementById('ofertas');
+ofertas.textContent = "20% descuento en productos seleccionados";
+ofertas.style.color = "#04fd";
+console.log('ofertas');
+
+//Array vacio//
+const productos = [ ]
+
+//Array de objetos//
+let productos_descuento =[
+{nombre: 'vanity', colores: ['rosa', 'verde',  'negro' ], precio_lista: '150000', precio_final:'150000'},
+{nombre: 'lotus_flower', colores: [ 'verde',  'negro' ], precio_lista: '190000',precio_final:'150000' },
+{nombre: 'redox', colores: ['rosa', 'verde',  'azul' ], precio_lista: '280000',precio_final:'150000' }
+];
+console.log('productos_descuento');
+
+precio_lista = prompt;
+console.log('precio_lista');
+
+ precio_final = precio_lista - precio_lista/5;
+console.log('precio_final');
+
+function buscadorProducto(nombre, precio_lista, precio_final, color){
+const resultadosBusqueda = [];
+
+}
+
+
+
+
