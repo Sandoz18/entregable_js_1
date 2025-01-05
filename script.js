c3c71354decb86cad5870f8d07dd446e02bc33fc
Irene <ireneimdiez@gmail.com>
2025-01-05 20:20:42 -0300

    entrega1

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 //declaro e inicializo la variable ofertas//
-const ofertas = document.getElementById('ofertas');
+let ofertas = document.getElementById('ofertas');
 ofertas.textContent = "20% descuento en productos seleccionados";
 ofertas.style.color = "#04fd";
+ofertas.style.fontFamily = "roboto-bold";
+ofertas.style.fontSize = "40px";
 console.log('ofertas');
 
-//Array vacio//
-const productos = [ ]
+//Array productos Exitentes//
+let productos = [];
+
+//Array vacio para ingresar productos//
+let productosNuevos = [ ]
 
 //Array de objetos//
-let productos_descuento =[
-{nombre: 'vanity', colores: ['rosa', 'verde',  'negro' ], precio_lista: '150000', precio_final:'150000'},
-{nombre: 'lotus_flower', colores: [ 'verde',  'negro' ], precio_lista: '190000',precio_final:'150000' },
-{nombre: 'redox', colores: ['rosa', 'verde',  'azul' ], precio_lista: '280000',precio_final:'150000' }
+let productosConDescuento =[
+{nombre: 'vanity', colores: ['rosa', 'verde',  'negro' ], precio_lista: '150000',},
+{nombre: 'lotus_flower', colores: [ 'verde',  'negro' ], precio_lista: '190000', },
+{nombre: 'redox', colores: ['rosa', 'verde',  'azul' ], precio_lista: '280000', }
 ];
-console.log('productos_descuento');
+console.log('productosConDescuento');
 
-precio_lista = prompt;
-console.log('precio_lista');
+precioLista = prompt;
+console.log('precioLista');
 
- precio_final = precio_lista - precio_lista/5;
-console.log('precio_final');
+ precioFinal = precioLista - precioLista/5;
+console.log('precioFinal');
 
 function buscadorProducto(nombre, precio_lista, precio_final, color){
 const resultadosBusqueda = [];
