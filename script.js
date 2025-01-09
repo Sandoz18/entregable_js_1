405bf3a5a2aeaed17b9db7ae9684402f8fbf0f55
Irene <ireneimdiez@gmail.com>
2025-01-08 23:00:00 -0300

    maquetado y estructura general de la página

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 //declaro e inicializo la variable ofertas//
 let ofertas = document.getElementById('ofertas');
-ofertas.textContent = "20% descuento en productos seleccionados";
-ofertas.style.color = "#04fd";
+
+//le doy estilo con propiedades de js"//
+ofertas.textContent = "descuentos de hasta el 20% en productos seleccionados";
+ofertas.style.color = "#ffffff";
 ofertas.style.fontFamily = "roboto-bold";
 ofertas.style.fontSize = "40px";
 console.log('ofertas');
 
 //Array productos Existentes//
-let productos = [ 
-{nombre: 'vanity', colores: ['rosa', 'verde',  'negro' ], precioLista: '15000', productId:'1231'},
-{nombre: 'lotus_flower', colores: [ 'verde',  'negro' ], precioLista: '19000', productId:'1238'},
-{nombre: 'redox', colores: ['rosa', 'verde',  'azul' ], precioLista: '28000', productId:'1235'},
-{nombre: 'summertime', colores: ['rosa', 'rojo',  'negro' ], precioLista: '1900', productId:'1234'},
-{nombre: 'focus', colores: [ 'verde',  'negro' ], precioLista: '260000', productId:'1232'},
-{nombre: 'rocket', colores: ['rosa', 'verde',  'violeta' ], precioLista: '38000', productId:'1239' },
-{nombre: 'shali', colores: ['rosa', 'verde',  'negro' ], precioLista: '50000', productId:'1244'},
-{nombre: 'bubble', colores: [ 'verde',  'negro' ], precioLista: '32000',  productId:'1245'},
-{nombre: 'linger', colores: ['negro', 'amarillo',  'azul' ], precioLista: '28000', productId:'1246' }
+let productos = [
+    { nombre: 'vanity', colores: ['rosa', 'verde', 'negro'], precioLista: 15000, productId: '1231', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'lotus_flower', colores: ['verde', 'negro'], precioLista: 19000, productId: '1238', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'redox', colores: ['rosa', 'verde', 'azul'], precioLista: 28000, productId: '1235', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'summertime', colores: ['rosa', 'rojo', 'negro'], precioLista: 1900, productId: '1234', descripcionProducto: 'Lentes de sol de acetato y plata'},
+    { nombre: 'focus', colores: ['verde', 'negro'], precioLista: 260000, productId: '1232', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'rocket', colores: ['rosa', 'verde', 'violeta'], precioLista: 38000, productId: '1239', descripcionProducto: 'Lentes de sol de acetato y plata'},
+    { nombre: 'shali', colores: ['rosa', 'verde', 'negro'], precioLista: 50000, productId: '1244', descripcionProducto: 'Lentes de sol de acetato y plata'},
+    { nombre: 'bubble', colores: ['verde', 'negro'], precioLista: 32000, productId: '1245', descripcionProducto: 'Lentes de sol de acetato y plata'},
+    { nombre: 'linger', colores: ['negro', 'amarillo', 'azul'], precioLista: 28000, productId: '1246', descripcionProducto: 'Lentes de sol de acetato y plata'}
 
 ];
 
+console.log(productos);
+
 //Array vacio para ingresar productos//
-let productosNuevos = [ ];
+let productosNuevos = [];
 
 //Array de objetos//
-let productosConDescuento =[
-
+let productosConDescuento = [
+    { nombre: 'vanity', colores: ['rosa', 'verde', 'negro'], precioLista: 15000, productId: '1231', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'lotus_flower', colores: ['verde', 'negro'], precioLista: 19000, productId: '1238', descripcionProducto: 'Lentes de sol de acetato y plata' },
+    { nombre: 'redox', colores: ['rosa', 'verde', 'azul'], precioLista: 28000, productId: '1235', descripcionProducto: 'Lentes de sol de acetato y plata' },
 ];
 console.log('productosConDescuento');
 
-precioLista = prompt;
-console.log('precioLista');
+let precioLista = parseInt(prompt);
+console.log(typeof precioLista);
 
- precioFinal = precioLista - precioLista/5;
-console.log('precioFinal');
+let porcentajeDescuento = precioLista*0.20;
 
-function buscadorProducto(nombre, precio_lista, precio_final, color){
-const resultadosBusqueda = [];
+precioFinalDescuento = precioLista - porcentajeDescuento;
+console.log("El precio del producto con descuento es:" + precioFinalDescuento);
 
+
+
+function buscadorProducto(nombre, precio_lista, precio_final, color) {
+    const resultadosBusqueda = [];
 }
 
 
